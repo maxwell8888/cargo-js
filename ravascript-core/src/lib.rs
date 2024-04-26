f4b7dff0b94530c40eaa468fc51c8f8d98dc1186
Max Unsted <maxunsted@gmail.com>
2024-04-26 16:20:25 +0100

    fix items being added to scopes

--- ravascript-core/src/lib.rs
+++ ravascript-core/src/lib.rs
@@ -5436,7 +5436,7 @@ fn extract_data_populate_item_definitions(
                 }
                 Item::ForeignMod(_) => todo!(),
                 Item::Impl(_) => {
-                    // TODO
+                    // TODO IMPORTANT currently we are adding top level impl blocks to `global_data.impl_blocks` in handle_item_impl(). It would be better to push (non-scoped) impl blocks here, so that they are already available if a method defined on the impl is called before the impl block itself is reached/parsed by `handle_item_impl()`. However we still need to find a way to solve this problem for the scoped impl blocks anyway. Leave it as is for now until we do some refactoring and deduplication, to avoid need to repeat a bunch of code here.
                 }
                 Item::Macro(_) => {}
                 Item::Mod(item_mod) => {}
