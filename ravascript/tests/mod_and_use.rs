ce6ca874ae7d2dcb399d366d1c6ee2c72bdb2faf
Max Unsted <maxunsted@gmail.com>
2024-07-08 02:06:06 +0100

    clean up js spacing in mod_and_use test

--- ravascript/tests/mod_and_use.rs
+++ ravascript/tests/mod_and_use.rs
@@ -19,115 +19,116 @@ async fn it_transpiles_crate_directory() {
     let actual = format_js(actual);
 
     let expected = format_js(
-        r#"// crate
-          function crate__duplicateName() {
-            return 10;
-          }
-          function main() {
-            crate__duplicateName();
-            utils__colors__duplicateName();
-            let thing = External.new();
-            let fido = new Green(true, 2);
-            fido.woof();
-          };
-
-          // foo_bar
-          class Internal {
-            constructor(age) {
-              this.age = age;
+        r#"
+            // crate
+            function crate__duplicateName() {
+                return 10;
             }
-          
-            addTen() {
-              return this.age + 10;
+            function main() {
+                crate__duplicateName();
+                utils__colors__duplicateName();
+                let thing = External.new();
+                let fido = new Green(true, 2);
+                fido.woof();
+            };
+  
+            // foo_bar
+            class Internal {
+                constructor(age) {
+                    this.age = age;
+                }
+              
+                addTen() {
+                    return this.age + 10;
+                }
             }
-          }
-          
-          class External {
-            constructor(sub, count) {
-              this.sub = sub;
-              this.count = count;
+            
+            class External {
+                constructor(sub, count) {
+                    this.sub = sub;
+                    this.count = count;
+                }
+              
+                static new() {
+                    return new External(new Internal(0), 9);
+                }
             }
-          
-            static new() {
-              return new External(new Internal(0), 9);
+  
+            // foo_bar::file_module_level_inline
+            function fileModuleLevelInline__duplicateName() {
+                return 12;
             }
-          }
-
-          // foo_bar::file_module_level_inline
-          function fileModuleLevelInline__duplicateName() {
-            return 12;
-          }
-
-          // foo_bar::file_module_level_inline::file_module_inline_sub
-          function fileModuleInlineSub__duplicateName() {
-            return 13;
-          }
-
-          // colors
-          const DOG_ACTIVITY = 5;
-          function colors__duplicateName() {
-            return 6;
-          }
-          function stuffFunction() {
-            return 4;
-          }
-
-          // colors::green
-          function green__duplicateName() {
-            return 3;
-          }
-          class Green {
-            constructor(fluffy, age) {
-              this.fluffy = fluffy;
-              this.age = age;
+  
+            // foo_bar::file_module_level_inline::file_module_inline_sub
+            function fileModuleInlineSub__duplicateName() {
+                return 13;
             }
-          
-            woof() {
-              function duplicateName() {
-                return 9;
-              }
-              console.assert(this.age === 2);
-              console.assert(green__duplicateName() === 3);
-              console.assert(duplicateName() === 9);
-              console.assert(sayHello() === 8);
-              console.assert(crate__duplicateName() === 10);
-              console.assert(sayHello() === 8);
-              console.assert(green__duplicateName() === 3);
-              console.assert(colors__duplicateName() === 6);
-              console.assert(utils__colors__duplicateName() === 7);
-              console.assert(DOG_ACTIVITY === 5);
-              console.assert(stuffFunction() === 4);
-              console.assert(crateLevelInline__duplicateName() === 1);
-              console.assert(crateLevelInlineSub__duplicateName() === 11);
-              console.assert(fileModuleLevelInline__duplicateName() === 12);
-              console.assert(fileModuleInlineSub__duplicateName() === 13);
+  
+            // colors
+            const DOG_ACTIVITY = 5;
+            function colors__duplicateName() {
+                return 6;
             }
-          }
-
-          // utils
-
-          // utils::say_something
-          function sayHello() {
-            return 8;
-          }
-
-          // utils::colors
-          function utils__colors__duplicateName() {
-            return 7;
-          }
-          
-          // crate_level_inline
-          function crateLevelInline__duplicateName() {
-            return 1;
-          }
-          
-          // crate_level_inline::crate_level_inline_sub
-          function crateLevelInlineSub__duplicateName() {
-            return 11;
-          }
-
-          main();
-          "#,
+            function stuffFunction() {
+                return 4;
+            }
+  
+            // colors::green
+            function green__duplicateName() {
+                return 3;
+            }
+            class Green {
+                constructor(fluffy, age) {
+                    this.fluffy = fluffy;
+                    this.age = age;
+                }
+              
+                woof() {
+                    function duplicateName() {
+                        return 9;
+                    }
+                    console.assert(this.age === 2);
+                    console.assert(green__duplicateName() === 3);
+                    console.assert(duplicateName() === 9);
+                    console.assert(sayHello() === 8);
+                    console.assert(crate__duplicateName() === 10);
+                    console.assert(sayHello() === 8);
+                    console.assert(green__duplicateName() === 3);
+                    console.assert(colors__duplicateName() === 6);
+                    console.assert(utils__colors__duplicateName() === 7);
+                    console.assert(DOG_ACTIVITY === 5);
+                    console.assert(stuffFunction() === 4);
+                    console.assert(crateLevelInline__duplicateName() === 1);
+                    console.assert(crateLevelInlineSub__duplicateName() === 11);
+                    console.assert(fileModuleLevelInline__duplicateName() === 12);
+                    console.assert(fileModuleInlineSub__duplicateName() === 13);
+                }
+            }
+  
+            // utils
+  
+            // utils::say_something
+            function sayHello() {
+                return 8;
+            }
+  
+            // utils::colors
+            function utils__colors__duplicateName() {
+                return 7;
+            }
+            
+            // crate_level_inline
+            function crateLevelInline__duplicateName() {
+                return 1;
+            }
+            
+            // crate_level_inline::crate_level_inline_sub
+            function crateLevelInlineSub__duplicateName() {
+                return 11;
+            }
+  
+            main();
+        "#,
     );
 
     // println!("{}", expected);
@@ -157,14 +158,14 @@ async fn simple_module() {
     );
     let expected = format_js(
         r#"
-          // crate
-          class Bar {}
-          function baz() {
-            let _ = new Bar();
-          }
-
-          // foo
-          function green() {}
+            // crate
+            class Bar {}
+            function baz() {
+                let _ = new Bar();
+            }
+  
+            // foo
+            function green() {}
         "#,
     );
     assert_eq!(expected, actual);
@@ -186,16 +187,20 @@ async fn module_super() {
             }
         }
     );
-    let expected = r#"// crate
-class Bar {}
-function baz() {
-  let _ = new Bar();
-}
-
-// foo
-function green() {
-  let blue = baz();
-}"#;
+    let expected = format_js(
+        r#"
+            // crate
+            class Bar {}
+            function baz() {
+                let _ = new Bar();
+            }
+            
+            // foo
+            function green() {
+                let blue = baz();
+            }
+        "#,
+    );
     assert_eq!(expected, actual);
     let _ = execute_js_with_assertions(&expected).await.unwrap();
 }
@@ -213,13 +218,13 @@ async fn module_self() {
     );
     let expected = format_js(
         r#"
-          class Bar {}
-          function baz() {
-            let _ = new Bar();
-          }
-          function green() {
-            let blue = baz();
-          }
+            class Bar {}
+            function baz() {
+                let _ = new Bar();
+            }
+            function green() {
+                let blue = baz();
+            }
         "#,
     );
     assert_eq!(expected, actual);
@@ -239,15 +244,19 @@ async fn module_crate() {
             }
         }
     );
-    let expected = r#"// crate
-function baz() {
-  return 5;
-}
-
-// foo
-function green() {
-  let blue = baz();
-}"#;
+    let expected = format_js(
+        r#"
+            // crate
+            function baz() {
+                return 5;
+            }
+            
+            // foo
+            function green() {
+                let blue = baz();
+            }
+        "#,
+    );
     assert_eq!(expected, actual);
     let _ = execute_js_with_assertions(&expected).await.unwrap();
 }
@@ -308,51 +317,152 @@ async fn use_paths() {
 
     let expected = format_js(
         r#"
-          // crate
-          function duplicate() {
-            return 0;
-          }
-          function main() {
-            console.assert(duplicate() === 0);
-            console.assert(one__duplicate() === 1);
-            console.assert(three__duplicate() === 3);
-            console.assert(four__duplicate() === 4);
-            console.assert(five__duplicate() === 5);
-          }
-
-          // one
-          function one__duplicate() {
-            return 1;
-          }
-
-          // one::two
-          function two__duplicate() {
-            return 2;
-          }
-
-          // one::two::three
-          function three__duplicate() {
-            return one__duplicate() + two__duplicate();
-          }
-
-          // one::two::three::another_one
-
-          // four
-          function four__duplicate() {
-            return three__duplicate() + one__duplicate();
-          }
-
-          // five
-          function five__duplicate() {
-            return three__duplicate() + one__duplicate() + one__duplicate();
-          }
-
-          main();
-      "#,
+            // crate
+            function duplicate() {
+                return 0;
+            }
+            function main() {
+                console.assert(duplicate() === 0);
+                console.assert(one__duplicate() === 1);
+                console.assert(three__duplicate() === 3);
+                console.assert(four__duplicate() === 4);
+                console.assert(five__duplicate() === 5);
+            }
+  
+            // one
+            function one__duplicate() {
+                return 1;
+            }
+  
+            // one::two
+            function two__duplicate() {
+                return 2;
+            }
+  
+            // one::two::three
+            function three__duplicate() {
+                return one__duplicate() + two__duplicate();
+            }
+  
+            // one::two::three::another_one
+  
+            // four
+            function four__duplicate() {
+                return three__duplicate() + one__duplicate();
+            }
+  
+            // five
+            function five__duplicate() {
+                return three__duplicate() + one__duplicate() + one__duplicate();
+            }
+  
+            main();
+        "#,
     );
 
     assert_eq!(expected, actual);
     let _ = execute_js_with_assertions(&actual).await.unwrap();
+}
+
+// TODO IMPORTANT scoped modules. I think scoped modules might break
+#[ignore = "TODO"]
+#[tokio::test]
+async fn scoped_modules() {
+    let actual = r2j_file_run_main!(
+        fn one() -> i32 {
+            mod foo {
+                pub fn bar() -> i32 {
+                    1
+                }
+            }
+            foo::bar()
+        }
+        // fn two() -> i32 {
+        //     mod foo {
+        //         pub fn bar() -> i32 {
+        //             2
+        //         }
+        //     }
+        //     foo::bar()
+        // }
+        fn main() {
+            assert!(one() == 1);
+            // assert!(two() == 2);
+        }
+    );
+
+    let expected = format_js(
+        r#"
+            // crate
+            function one() {
+                function bar() {
+                    return 1;
+                }
+                return bar();
+            }
+            function two() {
+                function bar() {
+                    return 1;
+                }
+                return bar();
+            }
+            function main() {
+              console.assert(one() === 1);
+              console.assert(two() === 2);
+            }
+
+            main();
+        "#,
+    );
+
+    assert_eq!(expected, actual);
+    let _ = execute_js_with_assertions(&expected).await.unwrap();
+}
+
+#[ignore = "TODO"]
+#[tokio::test]
+async fn scoped_modules_duplicate_names() {
+    let actual = r2j_file_run_main!(
+        fn foo() -> i32 {
+            mod one {
+                pub fn num() -> i32 {
+                    1
+                }
+            }
+            mod two {
+                pub fn num() -> i32 {
+                    2
+                }
+            }
+            one::num() + two::num()
+        }
+        fn main() {
+            assert!(foo() == 3);
+        }
+    );
+
+    let expected = format_js(
+        r#"
+            // crate
+            function foo() {
+                function one__num() {
+                    return 1;
+                }
+                function two__num() {
+                    return 2;
+                }
+                return one__num() + two__num();
+            }
+            function main() {
+              console.assert(foo() === 3);
+            }
+
+            main();
+        "#,
+    );
+
+    assert_eq!(expected, actual);
+    let _ = execute_js_with_assertions(&expected).await.unwrap();
 }
 
 #[ignore = "TODO"]
@@ -378,16 +488,16 @@ async fn scoped_use() {
         r#"
             // crate
             function duplicate() {
-              console.assert(one__duplicate() === 1);
-              return 5;
+                console.assert(one__duplicate() === 1);
+                return 5;
             }
             function main() {
-              duplicate();
+                duplicate();
             }
 
             // one
             function one__duplicate() {
-              return 1;
+                return 1;
             }
 
             main();
