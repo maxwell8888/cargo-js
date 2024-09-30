8ecb03348926faf37984893738c71da72adbfe5a
Max Unsted <maxunsted@gmail.com>
2024-10-01 00:34:38 +0100

    add to string escaping test

--- ravascript/tests/it/string_escaping.rs
+++ ravascript/tests/it/string_escaping.rs
@@ -6,17 +6,19 @@ use super::utils::*;
 use crate::r2j_block_with_prelude;
 
 #[tokio::test]
-async fn mutating_strings() {
+async fn string_escaping() {
     #[allow(unused_variables)]
     let actual = r2j_block_with_prelude!({
         let json = r#"{ "text": "hello", "num": 5 }"#;
         let json2 = "{ \"text\": \"hello\", \"num\": 5 }";
+        let json3 = "{ 'text': 'hello', 'num': 5 }";
     });
 
     let expected = format_js(
         r#"
             let json = "{ \"text\": \"hello\", \"num\": 5 }";
             let json2 = "{ \"text\": \"hello\", \"num\": 5 }";
+            let json3 = "{ 'text': 'hello', 'num': 5 }";
         "#,
     );
 
